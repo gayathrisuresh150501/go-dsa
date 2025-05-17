2170b5b8ced15ff4377680fef7bf2dd320f6b731
Gayathri Suresh <gayathri.suresh@ad.loginradius.net>
2025-05-17 21:12:43 +0530

    Add digit right triangle

--- pattern/triangle.go
+++ pattern/triangle.go
@@ -2,6 +2,7 @@ package pattern
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -10,7 +11,10 @@ func PrintTriangle(height int) {
 
 	for row := 1; row <= height; row += 1 {
 		for col := 1; col <= row; col += 1 {
-			buf.WriteString("* ")
+			// buf.WriteString("* ")
+			// buf.WriteString(strconv.Itoa(row)) // To print numbers incremented row wise
+			buf.WriteString(strconv.Itoa(col)) // To print numbers incremented col wise
+
 		}
 
 		buf.WriteString("\n")
